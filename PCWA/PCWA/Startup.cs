1efd43aceb64c40e033ddbbf6c87906a130a781c
Ferit Tenöz <ftenoz@gmail.com>
2018-09-12 11:45:11 +0300

    Initial Commit

---
+++ PCWA/PCWA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PCWA.Startup))]
+namespace PCWA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
